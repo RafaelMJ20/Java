c0923235e6fa100b520e58802f8072c6228a23c6
RAFAEL MIRANDA JIMENEZ <91084460+RafaelMJ20@users.noreply.github.com>
2021-10-05 14:46:55 -0500

    Add files via upload

---
+++ hola_mundo_java.java
@@ -0,0 +1,18 @@
+public class hola_mundo_java{
+	/**
+	 * Aplicacion java
+	 * Title	:Hola mundo en java
+	 * Description	:Hola mundo en java desde vim en windows
+	 * Author	:Rafael Miranda Jimenez 
+	 * Date 	:20211004
+	 * Version	:1
+	 * Usage	:C:\>javac /path/hola_mundo_java.java
+	 * 		:c:\>java hola_mundo_java
+	 */
+	
+
+	public static void main(String []args) {
+		System.out.println("Hola Mundo"); // prints Hola Mundo			
+	}	
+
+}
